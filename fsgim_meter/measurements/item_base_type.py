3ff170add5064ad39cd547b7865def5fe1b8a66c
Teja Vaitla <vaitla.kkd@gmail.com>
2018-02-12 14:31:59 +0530

    create ItemBaseType Class

---
+++ fsgim_meter/measurements/item_base_type.py
@@ -0,0 +1,9 @@
+"""
+    Define ItemBaseType Class
+"""
+
+class ItemBaseType:
+    """
+        Abstract base type for units for EMIX Product delivery, measurement, and warrants.
+    """
+    pass
